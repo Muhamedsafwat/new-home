c06e4dd9bad08fb426475b8057a549f4c83b3eb4
Shahd Mahmoud <158595641+eng-shahd@users.noreply.github.com>
2024-10-04 21:03:12 +0300

    Update video-scene.js

--- src/scenes/video-scene.js
+++ src/scenes/video-scene.js
@@ -1,24 +1,45 @@
+import { drawFullWidthBox } from '../utils.js';
+
 export class VideoScene extends Phaser.Scene {
   constructor() {
     super('VideoScene');
   }
 
   preload() {
-    this.load.video('introVideo', 'assets/vedio/intro.mp4');
+    this.load.video('introVideo', 'assets/vedio/v1.mp4'); 
   }
 
   create() {
-    const video = this.add.video(225, 320, 'introVideo');
+    
+    const video = this.add.video(0, 0, 'introVideo');
 
-    video.play(true);
+    
+    video.setOrigin(0.5, 0.5);
 
+
+
+    
+    video.setPosition(this.cameras.main.width / 2, this.cameras.main.height / 2); 
+
+    video.play(true); 
+
+    // Add text on top of the video
+    const text = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, "Let's go", {
+      font: '32px Arial', 
+      fill: '#ffffff', 
+      align: 'center', 
+    });
+    text.setOrigin(0.5, 0.5); 
+
+    
     video.on('complete', () => {
-      this.scene.start('GameScene');
+      this.scene.start('LevelOneScene');
     });
 
+    //  stop the video 
     this.input.once('pointerdown', () => {
       video.stop();
-      this.scene.start('GameScene');
+      this.scene.start('LevelOneScene');
     });
   }
 }
