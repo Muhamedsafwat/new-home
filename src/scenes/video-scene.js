aaaf5063686dc17f5f83fb51cb1173db4bea0ff6
Shahd Mahmoud <158595641+eng-shahd@users.noreply.github.com>
2024-10-04 23:24:55 +0300

    Update video-scene.js

--- src/scenes/video-scene.js
+++ src/scenes/video-scene.js
@@ -6,38 +6,32 @@ export class VideoScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.video('introVideo', 'assets/vedio/v1.mp4'); 
+    this.load.video('introVideo', 'assets/vedio/v1.mp4');
+    this.load.audio('clickSound', 'assets/audio/click.mp3'); // Load the click sound
   }
 
   create() {
-    
     const video = this.add.video(0, 0, 'introVideo');
 
-    
     video.setOrigin(0.5, 0.5);
-
-
-
-    
-    video.setPosition(this.cameras.main.width / 2, this.cameras.main.height / 2); 
-
-    video.play(true); 
+    video.setPosition(this.cameras.main.width / 2, this.cameras.main.height / 2);
+    video.play(true);
 
     // Add text on top of the video
     const text = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, "Let's go", {
-      font: '32px Arial', 
-      fill: '#ffffff', 
-      align: 'center', 
+      font: '32px Arial',
+      fill: '#ffffff',
+      align: 'center',
     });
-    text.setOrigin(0.5, 0.5); 
+    text.setOrigin(0.5, 0.5);
 
-    
     video.on('complete', () => {
       this.scene.start('LevelOneScene');
     });
 
-    //  stop the video 
+    // Play sound and stop the video on pointer down
     this.input.once('pointerdown', () => {
+      this.sound.play('clickSound'); // Play click sound
       video.stop();
       this.scene.start('LevelOneScene');
     });
