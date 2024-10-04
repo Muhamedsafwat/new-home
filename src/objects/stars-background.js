81d3cab231cbd43b7ea4c33d0dc274219eb4feaa
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-10-04 19:58:38 +0300

    update stars-background: make the background parallax

--- src/objects/stars-background.js
+++ src/objects/stars-background.js
@@ -1,6 +1,7 @@
 export class StarsBackground extends Phaser.GameObjects.Container {
   starsCountdown;
   starsGroup;
+  parallaxFactor = 0.2; // Factor to make stars move slower than the spaceship
 
   /** @param {Phaser.Scene} scene */
   constructor(scene) {
@@ -10,7 +11,8 @@ export class StarsBackground extends Phaser.GameObjects.Container {
     this.starsGroup = this.scene.add.group();
   }
 
-  backgroundObjsUpdate() {
+  // Update method that accepts spaceship velocity for parallax effect
+  backgroundObjsUpdate(spaceshipVelocity) {
     // update background objects
     this.starsGroup.getChildren().forEach((star) => {
       /** @type {Phaser.GameObjects.Image} */
@@ -18,43 +20,41 @@ export class StarsBackground extends Phaser.GameObjects.Container {
       // @ts-ignore
       const starObj = star; // Cast to Image or Sprite
       if (starObj.active) {
-        // stars move faster in foreground than in background layer
-        starObj.y -= starObj.scaleY * 1.2; 
+        // Move stars in the opposite direction of the spaceship's movement, slower by the parallax factor
+        starObj.y -= spaceshipVelocity.y * this.parallaxFactor;
+        starObj.x -= spaceshipVelocity.x * this.parallaxFactor;
 
-        // remove stars when they reach top of screen
-        if (starObj.y < -64) {
+        // Remove stars when they reach top of screen
+        if (starObj.y < -64 || starObj.y > this.scene.scale.height + 64 || starObj.x < -64 || starObj.x > this.scene.scale.width + 64) {
           starObj.setActive(false).setVisible(false); // Phaser 3 method to "kill"
         }
       }
     });
 
-    // add more stars?
+    // Add more stars
     this.starsCountdown -= 1;
     if (this.starsCountdown <= 0) {
       // add random background object
       var x = Phaser.Math.Between(0, this.scene.scale.width - 32);
-      var y = Phaser.Math.Between(0, this.scene.scale.height );
+      var y = Phaser.Math.Between(0, this.scene.scale.height);
       // recycle old background objects
       var newobj = this.starsGroup.getFirstDead(false);
 
       // if there aren't any available, create a new one
       if (!newobj) {
-      
         newobj = this.scene.add.image(x, y, "staralpha");
         this.starsGroup.add(newobj);
       } else {
-        
-
         newobj.setPosition(x, y);
         newobj.setActive(true).setVisible(true);
       }
 
-      var scale = Phaser.Math.FloatBetween(0.2, 0.8);
+      var scale = Phaser.Math.FloatBetween(0.2, 0.6);
       newobj.setScale(scale);
       newobj.health = scale; // for sorting
 
       // random countdown to new background object
-      this.starsCountdown = Phaser.Math.Between(15, 30);
+      this.starsCountdown = Phaser.Math.Between(15, 40);
     }
   }
 }
--- src/scenes/exploration-scene.js
+++ src/scenes/exploration-scene.js
@@ -2,8 +2,11 @@ import { SpaceShip } from "../objects/space-ship.js";
 import { StarsBackground } from "../objects/stars-background.js";
 
 export class ExplorationScene extends Phaser.Scene {
-  /** @type {StarsBackground}*/
+  /** @type {StarsBackground} */
   starsBackground;
+  /** @type {SpaceShip} */
+  spaceShip;
+
   constructor() {
     super("ExplorationScene");
   }
@@ -14,11 +17,17 @@ export class ExplorationScene extends Phaser.Scene {
   }
 
   create() {
+    // Create the stars background and spaceship objects
     this.starsBackground = new StarsBackground(this);
-    const spaceShip = new SpaceShip(this);
+    this.spaceShip = new SpaceShip(this);
   }
 
   update() {
-    this.starsBackground.backgroundObjsUpdate();
+    // Get the spaceship's velocity and pass it to the background update method
+    const spaceshipVelocity = this.spaceShip.getVelocity();
+    this.starsBackground.backgroundObjsUpdate(spaceshipVelocity);
+
+    // Update the spaceship
+    this.spaceShip.update();
   }
 }
